f272e27b3436ab15a033edfac24ce23bc4dbb5bd
Casaemiro <silasmagho@gmail.com>
2022-07-06 11:31:56 +0100

    complete

--- src/Email-desktop.js
+++ src/Email-desktop.js
@@ -12,31 +12,67 @@ import './Email-desktop-style.css'
 const Emaildesk = () => {
     return (
         <div className="emaildesk-section">
-            <table style={{}}>
+            <table style={{}} className="tab1">
                 <tr>
                     <td><img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/Kuelap_Ayuk-Etta-Image.svg?alt=media&token=d041cfe0-98a9-485f-ac18-7eeea6606529"} alt="" style={{ width: "250px" }} /></td>
-                    <td><img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/Kuelap_arrow.svg?alt=media&token=f86ead3f-7e9e-4d24-a003-4ca367e42b1b"} alt="" style={{ width: "68.5px", marginLeft: "20px"}} /></td>
-                    <td style={{ paddingLeft:"10px"}}>
+                    <td><img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/Kuelap_arrow.svg?alt=media&token=f86ead3f-7e9e-4d24-a003-4ca367e42b1b"} alt="" style={{ width: "68.5px", marginLeft: "20px" }} /></td>
+                    <td style={{ paddingLeft: "10px" }}>
                         <span style={{ color: "#6E90CA", fontWeight: "bold", fontSize: "1.2rem" }}>Ayuk Etta<br />CEO Kuelab Africa<br /></span>
                         <span style={{ fontSize: "1.2rem", fontWeight: "bold" }}>T:<a href="tel:+237691376601">(+237) 691 376 601</a><br />
                             W: <a href="https://wa.me/237691376601">691 376 601</a><br />
                             E: <a href="mailto:isaac.belambo@kuelab.africa">isaac.belombo@kuelap.africa</a><br />
                             <a href="https://kuelap.africa/">kuelap.africa | Yaounde, Cameroon</a><br />
                             <div style={{ display: "flex", flexDirection: "row", width: "340px", marginTop: "20px" }}>
-                                <a href="https://www.instagram.com/kuelap.africa/" style={{ width: "15%", marginRight: "4%", color:"black" }}><img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/Instagram.svg?alt=media&token=f2efcb8a-aec3-4928-8ef7-b73e6a6b50a4"} alt="" /></a>
-                                <a href="https://m.facebook.com/kuelap.africa" style={{ width: "15%", marginRight: "4%" }} ><img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/Facebook.svg?alt=media&token=ffe4e992-42a7-4f8f-9c33-fbfbb50a720f"} alt=""/></a>
+                                <a href="https://www.instagram.com/kuelap.africa/" style={{ width: "15%", marginRight: "4%", color: "black" }}><img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/Instagram.svg?alt=media&token=f2efcb8a-aec3-4928-8ef7-b73e6a6b50a4"} alt="" /></a>
+                                <a href="https://m.facebook.com/kuelap.africa" style={{ width: "15%", marginRight: "4%" }} ><img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/Facebook.svg?alt=media&token=ffe4e992-42a7-4f8f-9c33-fbfbb50a720f"} alt="" /></a>
                                 <a href="https://www.linkedin.com/company/kuelap-africa/" style={{ width: "15%", marginRight: "4%" }}><img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/LinkedIn.svg?alt=media&token=819e32d4-562e-4153-aedc-f2c3f8cea417"} alt="" /></a>
-                                <a href="https://twitter.com/Kuelap_Africa" style={{ width: "15%", marginRight: "4%" }} ><img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/Twitter.svg?alt=media&token=81a4fc05-7425-408c-b0ee-ae08b6799e3c"} alt=""/></a>
+                                <a href="https://twitter.com/Kuelap_Africa" style={{ width: "15%", marginRight: "4%" }} ><img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/Twitter.svg?alt=media&token=81a4fc05-7425-408c-b0ee-ae08b6799e3c"} alt="" /></a>
                             </div></span>
                     </td>
 
                 </tr>
                 <tr>
                     <td colspan="2">
-                        <img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/Kuelap_Logo_horizontal.svg?alt=media&token=fc5c7b64-7110-4ec3-858a-3dca89320e8e"} alt="" style={{ width: "310px" , marginLeft:"10px"}} />
+                        <img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/Kuelap_Logo_horizontal.svg?alt=media&token=fc5c7b64-7110-4ec3-858a-3dca89320e8e"} alt="" style={{ width: "310px", marginLeft: "10px" }} />
                     </td>
-                    <td style={{borderLeft:"3px solid #6E90CA", paddingLeft:"10px"}}>
-                    <span style={{ color: "#6E90CA", fontWeight: "bold", fontSize: "1.6rem" }}>Transforming Digital<br />Financial Services<br /></span>
+                    <td style={{ borderLeft: "3px solid #6E90CA", paddingLeft: "10px" }}>
+                        <span style={{ color: "#6E90CA", fontWeight: "bold", fontSize: "1.6rem" }}>Transforming Digital<br />Financial Services<br /></span>
+                    </td>
+                </tr>
+            </table>
+
+
+
+            {/* __________________________________________________________________________________________________________________________________________ */}
+
+
+            <table style={{}} className="tab2">
+                <tr>
+                    <td rowSpan="3"><img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/Kuelap_arrow.svg?alt=media&token=f86ead3f-7e9e-4d24-a003-4ca367e42b1b"} alt="" style={{ width: "68.5px", marginLeft: "20px" }} /></td>
+                    <td>
+                        <img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/Kuelap__Vertical-logo.svg?alt=media&token=c1d9b63c-d108-4e38-8f12-5db909933264"} alt="" style={{ width: "70px" }} />
+                    </td>
+                    <td style={{ borderLeft: "3px solid #6E90CA", paddingLeft: "10px", lineHeight:"11px" }}>
+                        <span style={{ color: "#6E90CA", fontWeight: "bold", fontSize: "0.8rem" }}>Transforming<br />Digital<br />Financial<br />Services<br /></span>
+                    </td>
+
+                </tr>
+                <tr>
+                <td colspan="2" ><img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/Kuelap_Ayuk-Etta-Image.svg?alt=media&token=d041cfe0-98a9-485f-ac18-7eeea6606529"} alt="" style={{ width: "140px" }} /></td>
+                </tr>
+                <tr>
+                <td style={{ paddingLeft: "10px" }} colspan="2">
+                        <span style={{ color: "#6E90CA", fontWeight: "bold", fontSize: "0.8rem" }}>Ayuk Etta<br />CEO Kuelab Africa<br /></span>
+                        <span style={{ fontSize: "0.7rem", fontWeight: "bold", lineHeight:"1px"  }}>T:<a href="tel:+237691376601">(+237) 691 376 601</a><br />
+                            W: <a href="https://wa.me/237691376601">691 376 601</a><br />
+                            E: <a href="mailto:isaac.belambo@kuelab.africa">isaac.belombo@kuelap.africa</a><br />
+                            <a href="https://kuelap.africa/">kuelap.africa | Yaounde, Cameroon</a><br />
+                            <div style={{ display: "flex", flexDirection: "row", width: "150px", marginTop: "10px" }}>
+                                <a href="https://www.instagram.com/kuelap.africa/" style={{ width: "15%", marginRight: "4%", color: "black" }}><img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/Instagram.svg?alt=media&token=f2efcb8a-aec3-4928-8ef7-b73e6a6b50a4"} alt="" /></a>
+                                <a href="https://m.facebook.com/kuelap.africa" style={{ width: "15%", marginRight: "4%" }} ><img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/Facebook.svg?alt=media&token=ffe4e992-42a7-4f8f-9c33-fbfbb50a720f"} alt="" /></a>
+                                <a href="https://www.linkedin.com/company/kuelap-africa/" style={{ width: "15%", marginRight: "4%" }}><img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/LinkedIn.svg?alt=media&token=819e32d4-562e-4153-aedc-f2c3f8cea417"} alt="" /></a>
+                                <a href="https://twitter.com/Kuelap_Africa" style={{ width: "15%", marginRight: "4%" }} ><img src={"https://firebasestorage.googleapis.com/v0/b/email-signature-716cb.appspot.com/o/Twitter.svg?alt=media&token=81a4fc05-7425-408c-b0ee-ae08b6799e3c"} alt="" /></a>
+                            </div></span>
                     </td>
                 </tr>
             </table>
